db44f7464d857db9a497c4751fad842eb78663af
lucasebs <lucasebatista@gmail.com>
2018-08-01 14:13:26 -0300

    Ajustes retornos e parametros Transfer. Adicionado getTipoTransfer()

--- src/br/com/portadosdesesperados/Transfer.java
+++ src/br/com/portadosdesesperados/Transfer.java
@@ -3,9 +3,9 @@ package br.com.portadosdesesperados;
 import java.io.IOException;
 
 public interface Transfer {
-    void transmitir(Integer chave, Object mensagem) throws IOException;
-    Object receber(Integer chave) throws IOException;
+    void transmitir(Integer chave, String mensagem) throws IOException;
+    String receber(Integer chave) throws IOException;
     void desconectar() throws IOException;
     void conectar() throws IOException;
-
+    String getTipoTransfer();
 }
