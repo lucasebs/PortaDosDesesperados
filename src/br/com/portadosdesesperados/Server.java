a714d783a284c050375fae4156e87d9facfe3a76
lucasebs <lucasebatista@gmail.com>
2018-08-01 14:15:40 -0300

    Adicionado tipoTransfer e ajustes no método conectar()

--- src/br/com/portadosdesesperados/Server.java
+++ src/br/com/portadosdesesperados/Server.java
@@ -10,32 +10,57 @@ public class Server implements Transfer {
     private Socket client;
     private InputStream in;
     private BufferedReader bin;
-
+    private String tipoTransfer = "Servidor";
 
     @Override
-    public void transmitir(Integer chave,Object mensagem) throws IOException {
-        if (chave == 0) {
-            PrintWriter pout = new PrintWriter(this.client.getOutputStream(), true);
-            pout.println(mensagem);
-        }
-        if (chave == 0) {
+    public void transmitir(Integer chave, String mensagem) throws IOException {
+        PrintWriter pout = new PrintWriter(this.client.getOutputStream(), true);
+        pout.println(mensagem);
 
-        }
+//        if (chave == 0) {
+//            PrintWriter pout = new PrintWriter(this.client.getOutputStream(), true);
+//            pout.println(mensagem);
+//        }
+//        if (chave == 1) {
+////            ObjectOutputStream output = new ObjectOutputStream(this.client.getOutputStream());
+////            output.writeObject(mensagem);
+//            PrintWriter pout = new PrintWriter(this.client.getOutputStream(), true);
+//            pout.println(mensagem);
+//        }
     }
 
 
     @Override
-    public Object receber(Integer chave) throws IOException {
-        if (chave == 0){
-            this.in = this.client.getInputStream();
-            if (in == null){
-                return null;
-            }
-            this.bin = new BufferedReader(new InputStreamReader(this.in));
-            String line = this.bin.readLine();
-            return line + "\n";
+    public String receber(Integer chave) throws IOException {
+        this.in = this.client.getInputStream();
+        if (in == null){
+            return null;
         }
-        return null;
+        this.bin = new BufferedReader(new InputStreamReader(this.in));
+        String line = this.bin.readLine();
+        return line;
+//        this.in = this.client.getInputStream();
+//        if (in == null){
+//            return null;
+//        }
+//        this.bin = new BufferedReader(new InputStreamReader(this.in));
+//        if (chave == 0){
+////            this.bin = new BufferedReader(new InputStreamReader(this.in));
+//            String line = this.bin.readLine();
+//            return line + "\n";
+//        }
+//        if (chave == 1){
+//            ObjectInputStream input = new ObjectInputStream(this.in);
+//            Object objInput = null;
+//            try {
+//                objInput = input.readObject();
+//            } catch (ClassNotFoundException e) {
+//                System.out.println(e.getMessage());
+////                e.printStackTrace();
+//            }
+//            return objInput;
+//        }
+//        return null;
     }
 
     @Override
@@ -47,10 +72,15 @@ public class Server implements Transfer {
     public void conectar() throws IOException {
         System.out.println("== Servidor Iniciado ==\n");
         this.sock = new ServerSocket(6013);
-        System.out.println("== Servidor Iniciado ==\n");
+//        System.out.println("== Servidor Iniciado ==\n");
         this.client = this.sock.accept();
+        System.out.println("=== Servidor Conectado ===\n");
         System.out.println("Cliente conectado do IP "+ this.client.getInetAddress().getHostAddress());
-        System.out.println("== Servidor Iniciado ==\n");
+
+    }
+
+    public String getTipoTransfer() {
+        return tipoTransfer;
     }
 }
 
